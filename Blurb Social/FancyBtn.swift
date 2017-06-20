2af43caed895ad5a842f67f94b348c1a64a5f405
Nikka-Sher <npiterman@gmail.com>
2017-06-19 18:47:08 -0700

    sign in UI custom classes complete

---
+++ Blurb Social/FancyBtn.swift
@@ -0,0 +1,25 @@
+//
+//  FancyBtn.swift
+//  Blurb Social
+//
+//  Created by Konstantine Piterman on 6/19/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class FancyBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 4.0
+
+}
+    
+}
