0d6c4a3e50fbfc57b2d1ac6c7f82b9cd18525d69
Nikka-Sher <npiterman@gmail.com>
2017-06-19 18:24:59 -0700

    shadowed edges of view

---
+++ Blurb Social/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Blurb Social
+//
+//  Created by Konstantine Piterman on 6/19/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import Foundation
---
+++ Blurb Social/FancyView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyView.swift
+//  Blurb Social
+//
+//  Created by Konstantine Piterman on 6/19/17.
+//  Copyright © 2017 Konstantine Piterman. All rights reserved.
+//
+
+import UIKit
+
+class FancyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
