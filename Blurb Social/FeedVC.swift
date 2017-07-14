c65b31318374d2d437f862b3883c5f87a66dc306
Nikka-Sher <npiterman@gmail.com>
2017-07-14 12:34:11 -0700

    completed firebase caption/likes

--- Blurb Social/FeedVC.swift
+++ Blurb Social/FeedVC.swift
@@ -56,9 +56,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+ 
         let post = posts[indexPath.row]
-        print("NIKKA: \(post.caption)")
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
         
     }
 
