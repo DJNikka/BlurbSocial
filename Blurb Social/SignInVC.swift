a4adb5920ba999cfcc0bb1625ccd2c2f3103933f
Nikka-Sher <npiterman@gmail.com>
2017-07-13 20:19:06 -0700

    completed creating firebase database users

--- Blurb Social/SignInVC.swift
+++ Blurb Social/SignInVC.swift
@@ -57,7 +57,9 @@ class SignInVC: UIViewController {
             
                 print("NIKKA: Succesfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 
     
                 }
@@ -76,7 +78,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("NIKKA: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                     
                 } else {
@@ -86,7 +89,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("NIKKA: Successfully authenticated with Firebase")
                             if let user = user {
-                            self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                            self.completeSignIn(id: user.uid, userData: userData)
                         
                         }
                         }
@@ -99,7 +103,8 @@ class SignInVC: UIViewController {
     }
     
 
-        func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+    DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
             
             let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
             print("NIKKA: Data saved to keychain \(keychainResult)")
